df4f9a8eb79ea7d04a8a23fe1175df4d2f0d9712
Lucas <lucas.levasseur18@gmail.com>
2024-11-13 21:15:39 -0300

    Feat useStateHook

---
+++ src/useState-Page/useStateHook.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import './useStateHook.css';
+import { Navigate, useNavigate } from "react-router-dom";
+
+
+const useStateHook = () => {
+    const [ count, setCount ] = useState(0);
+
+    const navigate = useNavigate();
+
+    const incrementCount = () =>{
+        setCount((prevState) => prevState + 1)
+    };
+
+    const handleNext = (e) =>{
+        e.preventDefault();
+        navigate('/useEffect')
+  };
+  return (
+    <div>
+        <h1>{count}</h1>
+        <button onClick={incrementCount}>increment</button>
+        <button onClick={handleNext}>Next Hook</button>
+    </div>
+  )
+}
+
+export default useStateHook;
